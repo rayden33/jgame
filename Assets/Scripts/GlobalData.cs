8e67243519ae5ca34e46127269342948af62ad85
Doniyorjon <rayden33@mail.ru>
2019-11-16 16:08:53 +0500

    globalization_1

--- Assets/Scripts/GlobalData.cs
+++ Assets/Scripts/GlobalData.cs
@@ -8,7 +8,7 @@ public static class GlobalData
     
     private static string actionCode;
 
-    // functions std
+    // functions utils
     public static string get_action_code(string elementTag)
     {
         return string.Concat(elementTag[0], elementTag[1]);
@@ -17,8 +17,40 @@ public static class GlobalData
     {
         return string.Concat(elementTag[2], elementTag[3],elementTag[4]);
     }
+
+    // main functions
+    static void take(RaycastHit hit)
+    {
+        string eCode = get_element_code(hit.transform.tag);
+        Object.Destroy(hit.transform.gameObject);
+        try {
+            inventoryElements[eCode]+=1;
+        }
+        catch {
+            inventoryElements.Add(eCode,1);
+        }
+    }
+    static void shoot(RaycastHit hit)
+    {
+        string eCode = get_element_code(hit.transform.tag);
+        Object.Destroy(hit.transform.gameObject);
+        try {
+            inventoryElements[eCode]+=1;
+        }
+        catch {
+            inventoryElements.Add(eCode,1);
+        }
+    }
     public static void doAction(RaycastHit hit)
     {
         actionCode = get_action_code(hit.transform.tag);
+        Debug.Log(actionCode);
+        switch(actionCode)
+        {
+            case "00": take(hit);
+            break;
+            case "01": shoot(hit);
+            break;
+        }
     }
 }
