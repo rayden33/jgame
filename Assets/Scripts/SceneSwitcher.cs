ea680a54d719ee878e1f2a37fc097cd022588484
AnvarovB <bekanvarov95@mail.ru>
2019-11-16 13:24:45 +0500

    UI
    new UI inventory

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void GotoMainScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void GotoMenuScene()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+}
