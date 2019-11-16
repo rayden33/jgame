329775d5841e6ac9ce426b30b74521a2a762e65f
Doniyorjon <rayden33@mail.ru>
2019-11-16 15:38:20 +0500

    delete_inventory_master

--- Assets/Scripts/GlobalData.cs
+++ Assets/Scripts/GlobalData.cs
@@ -5,4 +5,20 @@ using UnityEngine;
 public static class GlobalData
 {
     public static Dictionary<string, int> inventoryElements = new Dictionary<string, int>();
+    
+    private static string actionCode;
+
+    // functions std
+    public static string get_action_code(string elementTag)
+    {
+        return string.Concat(elementTag[0], elementTag[1]);
+    }
+    public static string get_element_code(string elementTag)
+    {
+        return string.Concat(elementTag[2], elementTag[3],elementTag[4]);
+    }
+    public static void doAction(RaycastHit hit)
+    {
+        actionCode = get_action_code(hit.transform.tag);
+    }
 }
